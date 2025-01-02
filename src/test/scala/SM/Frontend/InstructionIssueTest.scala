db2e1284c3b84c219840caa206f0d28ad6aeb8c4
Ari <arijusgrotuzas@gmail.com>
2025-01-02 19:50:57 +0100

    WIP

--- src/test/scala/SM/Frontend/InstructionIssueTest.scala
+++ src/test/scala/SM/Frontend/InstructionIssueTest.scala
@@ -4,23 +4,33 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-// TODO: Finish this test
 class InstructionIssueTest extends AnyFlatSpec with ChiselScalatestTester {
-  def pushInstruction() = {
-
+  def pushInstruction(dut: InstructionIssue, bitFields: (Int, Int, Int, Int, Int, Int)): Unit = {
+    dut.io.id.opcode.poke(bitFields._1.U)
+    dut.io.id.dest.poke(bitFields._2.U)
+    dut.io.id.rs1.poke(bitFields._3.U)
+    dut.io.id.rs2.poke(bitFields._4.U)
+    dut.io.id.rs3.poke(bitFields._5.U)
+    dut.io.id.imm.poke(bitFields._6.S)
   }
 
-  def expectInstruction(dut: InstructionIssue, opcode: Int, dest: Int, rs1: Int, rs2: Int, rs3: Int, imm: Int) = {
-    dut.io.iss.opcode.expect(2.U)
-    dut.io.iss.dest.expect(8.U)
-    dut.io.iss.rs1.expect(12.U)
-    dut.io.iss.rs2.expect(13.U)
-    dut.io.iss.rs3.expect(14.U)
-    dut.io.iss.imm.expect(255.S)
+  def expectBitFields(dut: InstructionIssue, bitFields: (Int, Int, Int, Int, Int, Int)): Unit = {
+    dut.io.iss.opcode.expect(bitFields._1.U)
+    dut.io.iss.dest.expect(bitFields._2.U)
+    dut.io.iss.rs1.expect(bitFields._3.U)
+    dut.io.iss.rs2.expect(bitFields._4.U)
+    dut.io.iss.rs3.expect(bitFields._5.U)
+    dut.io.iss.imm.expect(bitFields._6.S)
   }
 
   "InstructionIssue" should "push and pop correct instructions from queues" in {
     test(new InstructionIssue(4, 2)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      val inputBitFields = List(
+        (1, 5, 7, 8, 9, 32),
+        (2, 8, 12, 13, 14, 255),
+        (3, 19, 20, 21, 22, 112)
+      )
+
       // Default assignments
       dut.io.id.valid.poke(false.B)
       dut.io.id.pc.poke(0.U)
@@ -39,74 +49,42 @@ class InstructionIssueTest extends AnyFlatSpec with ChiselScalatestTester {
 
       dut.clock.step(4)
 
-      // Push the first instruction for the first warp to the queue
-      dut.io.scheduler.warp.poke(3.U)
+      // Set the push instruction warp and set the scheduler warp
       dut.io.id.valid.poke(true.B)
       dut.io.id.warp.poke(0.U)
-      dut.io.id.opcode.poke(1.U)
-      dut.io.id.dest.poke(5.U)
-      dut.io.id.rs1.poke(7.U)
-      dut.io.id.rs2.poke(8.U)
-      dut.io.id.rs3.poke(9.U)
-      dut.io.id.imm.poke(32.S)
+      dut.io.scheduler.warp.poke(3.U)
+
+      // Push the first instruction for the first warp to the queue
+      pushInstruction(dut, inputBitFields.head)
 
       dut.clock.step(1)
 
-      dut.io.scheduler.warp.poke(3.U)
-
       // Push the second instruction for the first warp to the queue
-      dut.io.id.warp.poke(0.U)
-      dut.io.id.opcode.poke(2.U)
-      dut.io.id.dest.poke(8.U)
-      dut.io.id.rs1.poke(12.U)
-      dut.io.id.rs2.poke(13.U)
-      dut.io.id.rs3.poke(14.U)
-      dut.io.id.imm.poke(255.S)
+      pushInstruction(dut, inputBitFields(1))
 
       dut.clock.step(1)
 
       // Push the third instruction for the first warp to the queue
-      dut.io.id.warp.poke(0.U)
-      dut.io.id.opcode.poke(3.U)
-      dut.io.id.dest.poke(19.U)
-      dut.io.id.rs1.poke(20.U)
-      dut.io.id.rs2.poke(21.U)
-      dut.io.id.rs3.poke(22.U)
-      dut.io.id.imm.poke(112.S)
+      pushInstruction(dut, inputBitFields(2))
 
       dut.clock.step(1)
 
-      // Push an invalid instruction
-      // Pop the first instruction from the first queue
       dut.io.id.valid.poke(false.B)
       dut.io.id.warp.poke(0.U)
-      dut.io.id.opcode.poke(0.U)
       dut.io.scheduler.warp.poke(0.U)
 
-      dut.io.iss.opcode.expect(1.U)
-      dut.io.iss.dest.expect(5.U)
-      dut.io.iss.rs1.expect(7.U)
-      dut.io.iss.rs2.expect(8.U)
-      dut.io.iss.rs3.expect(9.U)
-      dut.io.iss.imm.expect(32.S)
+      // Expect the first instruction to be popped from the first warp queue
+      expectBitFields(dut, inputBitFields.head)
 
       dut.clock.step(1)
 
-      dut.io.iss.opcode.expect(2.U)
-      dut.io.iss.dest.expect(8.U)
-      dut.io.iss.rs1.expect(12.U)
-      dut.io.iss.rs2.expect(13.U)
-      dut.io.iss.rs3.expect(14.U)
-      dut.io.iss.imm.expect(255.S)
+      // Expect the second instruction to be popped from the first warp queue
+      expectBitFields(dut, inputBitFields(1))
 
       dut.clock.step(1)
 
-      dut.io.iss.opcode.expect(3.U)
-      dut.io.iss.dest.expect(19.U)
-      dut.io.iss.rs1.expect(20.U)
-      dut.io.iss.rs2.expect(21.U)
-      dut.io.iss.rs3.expect(22.U)
-      dut.io.iss.imm.expect(112.S)
+      // Expect the third instruction to be popped from the first warp queue
+      expectBitFields(dut, inputBitFields(2))
 
       dut.clock.step(1)
     }
